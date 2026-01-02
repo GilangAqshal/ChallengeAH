88ce82a40792582e85c49ced6c325a4137747f23
GilangAqshal <gilangais16@gmail.com>
2026-01-02 23:06:40 +0700

    Challange 1 done

---
+++ logicLiterals.js
@@ -0,0 +1,10 @@
+const name = "Gilang Aqshal";
+const age = 19;
+const city = "Bekasi City";
+
+hasSim = false;
+if (hasSim){
+    console.log(`Hallo, Nama Saya ${name} berumur ${age} tahun dan saya boleh menyetir mobil di ${city}`);
+}else{
+    console.log(`Maaf ${name}, kamu belom boleh menyetir`);
+}
