cd0c1c8286e4e33bbeef8bb1d00f0e18debd9bb8
GilangAqshal <gilangais16@gmail.com>
2026-01-02 22:50:24 +0700

    FIrst COmmit

---
+++ app.js
@@ -0,0 +1,62 @@
+// string & variabel
+// const namaDepan = 'Gilang';
+// const namaBelakang = 'Aqshal';
+// const fullName = namaDepan + namaBelakang;
+
+// console.log(namaDepan);
+// console.log(namaBelakang);
+// console.log(fullName);
+
+// string concat
+// const namaDepan = 'Gilang';
+// const namaBelakang = 'Aqshal';
+// const fullName = namaDepan + namaBelakang;
+// const favoriteFood = 'Dada Ayam';
+// const text = "Halo namaku " + fullName + " dan makanan favoriteku adalah " + favoriteFood;
+// console.log(text);
+
+// number
+// const apel = 5000;
+// const pisang = 10000;
+// const diskon = 10000;
+// const customer = (apel * 3) + (pisang * 2) - diskon;
+// const Total = 'Total Belanja Customer : ' + customer;
+// console.log(Total);
+
+// boolean
+// is = adalah 
+// can = bisa
+// has = memiliki
+const hasChildren = false;
+const isStudent = true;
+const isEmployed = false;
+const canRideBike = true;
+const hasPets = false;
+
+const isTall = true;
+const canCook = true;
+const hasSiblings = false;
+
+// conditional
+const hasPasport = false;
+if(hasPasport){
+    console.log("Kamu boleh ke luar negeri");
+}else {
+    console.log("kamu tidak boleh keluar negeri");
+}
+
+const produk = false;
+if(produk){
+    console.log("Produk masih tersedia");
+}else {
+    console.log("Produk sudah habis");
+}
+
+// opertator comparision
+const value = 1 === 1;
+console.log(value);
+
+const abjad = 'cab' === "cba";
+console.log(abjad);
+
+
