fd194ec65652d3ccb6cdfeaf783ff9561ebf96d8
GilangAqshal <gilangais16@gmail.com>
2026-01-05 21:56:30 +0700

    Challenge 4 Done

---
+++ comFunction.js
@@ -0,0 +1,17 @@
+// function sapaLog(nama) {
+//   console.log(`Halo, ${nama}!`);
+// }
+
+// sapaLog("Gilang");
+
+// const hasil = sapaLog("Gilang");
+// console.log(hasil);
+
+function sapaReturn(nama){
+    return `Halo ${nama}!`;
+}
+
+sapaReturn("Gilang");
+
+const pesan = sapaReturn("Gilang");
+console.log(pesan);
