7e95f575bdce7a513f78673a3aab166c81424299
GilangAqshal <gilangais16@gmail.com>
2026-01-25 21:20:45 +0700

    Absen

---
+++ Januari/Dicoding/qualityCode.js
@@ -0,0 +1,18 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { add } from './calculator.mjs';
+ 
+describe('Calculator', () => {
+  it('should add correctly', () => {
+    // Arrange
+    const operandA = 1;
+    const operandB = 1;
+ 
+    // Action
+    const actualValue = add(operandA, operandB);
+ 
+    // Assert
+    const expectedValue = 2;
+    assert.equal(actualValue, expectedValue);
+  });
+});
