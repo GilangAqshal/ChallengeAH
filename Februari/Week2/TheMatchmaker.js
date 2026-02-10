3a4df26cdfc203bebd0f63d8cb3b7571664b869b
GilangAqshal <gilangais16@gmail.com>
2026-02-10 23:12:15 +0700

    The Matchmaker (Basic Logic)

--- Februari/Week2/TheMatchmaker.js
+++ Februari/Week2/TheMatchmaker.js
@@ -4,27 +4,12 @@
 // Output: "Match Result: [true/false]"
 
 // Tidak menggunakan parameter
-// const name = 'Gilang';
-// const hobby = 80;
-// const name1 = 'FIgo';
-// const hobby1 = 70;
-
-// const isCompatible = () => { 
-//     const average = (hobby + hobby1) / 2;
-//     if (average >= 75) {
-//             return `${name} dan ${name1} cocok karena rata-rata skor mereka adalah ${average}.`;
-//         } else {
-//             return `${name} dan ${name1} tidak cocok karena rata-rata skor hanya ${average}.`;
-//         }
-// }
-// console.log(isCompatible())
-
-// menggunakan parameter
 const name = 'Gilang';
+const hobby = 80;
 const name1 = 'FIgo';
+const hobby1 = 70;
 
-
-const isCompatible = (hobby, hobby1) => { 
+const isCompatible = () => { 
     const average = (hobby + hobby1) / 2;
     if (average >= 75) {
             return `${name} dan ${name1} cocok karena rata-rata skor mereka adalah ${average}.`;
@@ -32,4 +17,19 @@ const isCompatible = (hobby, hobby1) => {
             return `${name} dan ${name1} tidak cocok karena rata-rata skor hanya ${average}.`;
         }
 }
-console.log(isCompatible(90,76))
+console.log(isCompatible())
+
+// menggunakan parameter
+// const name = 'Gilang';
+// const name1 = 'FIgo';
+
+
+// const isCompatible = (hobby, hobby1) => { 
+//     const average = (hobby + hobby1) / 2;
+//     if (average >= 75) {
+//             return `${name} dan ${name1} cocok karena rata-rata skor mereka adalah ${average}.`;
+//         } else {
+//             return `${name} dan ${name1} tidak cocok karena rata-rata skor hanya ${average}.`;
+//         }
+// }
+// console.log(isCompatible(90,76));
