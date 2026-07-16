94cd4746059aa6ce1f287870c48f644362d61762
GilangAqshal <gilangais16@gmail.com>
2026-07-16 15:02:40 +0700

    es56

---
+++ Juli/es56/index.js
@@ -0,0 +1,8 @@
+// Cara Lama (ES5)
+function hitungDiskon(harga, persen) {
+  return harga * (persen / 100);
+}
+
+hitungDiskon(50000, 10);
+
+const hitungDiskonES6 = (harga, persen) => harga * (percent / 100);
---
+++ Juli/es56/praktik1.js
@@ -0,0 +1,10 @@
+// 💻 Tantangan Praktik 1:
+// Ubah fungsi ES5 di bawah ini menjadi Arrow Function yang ringkas (implicit return jika memungkinkan):
+// function sapaUser(nama) {
+//   return "Halo, " + nama + "!";
+// }
+
+const sapaUser = (nama) => `Halo, ${nama}!`;
+
+const gilang = "Gilang";
+sapaUser(gilang);
