42ec6785c72fc676a586effd17e1814ecfa32b55
GilangAqshal <gilangais16@gmail.com>
2026-07-16 15:13:25 +0700

    es6 litreal

---
+++ Juli/es56/praktik2.js
@@ -0,0 +1,12 @@
+const username = "Gilang";
+const role = "Developer";
+
+// Cara Baru (ES6)
+const bio = `User ${username} adalah seorang ${role}`;
+console.log(bio); // Output: User Gilang adalah seorang Developer
+
+const idProduk = 125;
+const kategori = "sepatu";
+
+const url = `https://toko.com/kategoti/${kategori}/produk/${idProduk}`;
+console.log(url);
