c28242cc4fa1c878fc816d0960f64beee6fcaf52
GilangAqshal <gilangais16@gmail.com>
2026-07-17 16:00:37 +0700

    state & button on click

--- Juli/belajar-react/src/App.jsx
+++ Juli/belajar-react/src/App.jsx
@@ -1,33 +1,59 @@
-import React from "react";
+// Use state
+import React, { useState } from "react";
 
-const Produk = ({ namaProduk, harga }) => {
+const TombolLike = () => {
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
-    <div className="card">
-      <h2>This is {namaProduk}!</h2>
-      <p>
-        Produk {namaProduk} sangat amat aman untuk pemula karena bagus untuk
-        membangun massa otot
-      </p>
-      <br />
-      <h2>Bill {harga}!</h2>
-      <p>
-        Harganya {harga} untuk sebuah {namaProduk}
-      </p>
+    <div style={{ marginTop: "10px" }}>
+      <button onClick={() => setIsLiked(!isLiked)}>
+        {/* Gunakan Ternary Operator untuk mengubah teks tombol secara dinamis */}
+        {isLiked ? "Sudah disukai ❤️" : "Sukai 🤍"}
+      </button>
     </div>
   );
 };
 
-// Komponen Utama (Parent Component)
 const App = () => {
   return (
     <div>
-      <Produk namaProduk="Muscle Fisrt Creatine" harga="170.000" />
-      {/* <Produk harga="Rp.170.000" /> */}
+      <TombolLike />
     </div>
   );
 };
 export default App;
 
+// Komponen dasar dan prop
+
+// import React from "react";
+// const Produk = ({ namaProduk, harga }) => {
+//   return (
+//     <div className="card">
+//       <h2>This is {namaProduk}!</h2>
+//       <p>
+//         Produk {namaProduk} sangat amat aman untuk pemula karena bagus untuk
+//         membangun massa otot
+//       </p>
+//       <br />
+//       <h2>Bill {harga}!</h2>
+//       <p>
+//         Harganya {harga} untuk sebuah {namaProduk}
+//       </p>
+//     </div>
+//   );
+// };
+
+// Komponen Utama (Parent Component)
+// const App = () => {
+//   return (
+//     <div>
+//       <Produk namaProduk="Muscle Fisrt Creatine" harga="170.000" />
+//     </div>
+//   );
+// };
+// export default App;
+
+// default
 // const App = () => {
 //   return (
 //     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
