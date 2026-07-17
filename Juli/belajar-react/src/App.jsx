af5a4bf76378bf4ded1f5ff4d2b5f9f3943f4d9c
GilangAqshal <gilangais16@gmail.com>
2026-07-17 14:57:08 +0700

    Komponen dasar dan props

--- Juli/belajar-react/src/App.jsx
+++ Juli/belajar-react/src/App.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 
-// const App = () => {
-//   return (
-//     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
-//       <h1>Selamat Datang di Belajar React!</h1>
-//       {/* Tempatkan komponen tantangan kamu di bawah sini */}
-//     </div>
-//   );
-// };
-
-const Produk = ({ namaProduk }) => {
+const Produk = ({ namaProduk, harga }) => {
   return (
     <div className="card">
       <h2>This is {namaProduk}!</h2>
       <p>
         Produk {namaProduk} sangat amat aman untuk pemula karena bagus untuk
         membangun massa otot
+      </p>
+      <br />
+      <h2>Bill {harga}!</h2>
+      <p>
+        Harganya {harga} untuk sebuah {namaProduk}
       </p>
     </div>
   );
@@ -25,8 +21,18 @@ const Produk = ({ namaProduk }) => {
 const App = () => {
   return (
     <div>
-      <Produk namaProduk="Muscle Fisrt Creatine" />
+      <Produk namaProduk="Muscle Fisrt Creatine" harga="170.000" />
+      {/* <Produk harga="Rp.170.000" /> */}
     </div>
   );
 };
 export default App;
+
+// const App = () => {
+//   return (
+//     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+//       <h1>Selamat Datang di Belajar React!</h1>
+//       {/* Tempatkan komponen tantangan kamu di bawah sini */}
+//     </div>
+//   );
+// };
