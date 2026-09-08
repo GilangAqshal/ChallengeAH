4d0e135ad3123dd46e1066e1f65b70f150a926b4
GilangAqshal <gilangais16@gmail.com>
2026-09-08 14:24:12 +0700

    The Alibi Contradiction

---
+++ Agustus/JSDetektif/chapter2.js
@@ -0,0 +1,30 @@
+// The Alibi Contradiction
+
+// data tersangka
+const suspect = [
+  { name: "Deni", location: "Zona Utama", accessTime: 22.25, securityLevel: 2 },
+  { name: "Eka", location: "Ruang Kantor", accessTime: 22.3, securityLevel: 3 },
+  { name: "Fajar", location: "Zona Utama", accessTime: 21.5, securityLevel: 3 },
+];
+
+// paramter waktu dan lokasi
+const crimeLocation = "Zona Utama";
+const crimeTimeStart = 22.15;
+const crimeTimeEnd = 22.45;
+const requireSecurityLevel = 3;
+
+suspect.forEach((tersangka) => {
+  if (
+    tersangka.location === crimeLocation &&
+    tersangka.accessTime >= crimeTimeStart &&
+    tersangka.accessTime <= crimeTimeEnd
+  ) {
+    if (tersangka.securityLevel < requireSecurityLevel) {
+      console.log("Tersangka Utama " + tersangka.name);
+    } else {
+      console.log("Akses sah diTKP " + tersangka.name);
+    }
+  } else {
+    console.log("Alibi Valid " + tersangka.name);
+  }
+});
